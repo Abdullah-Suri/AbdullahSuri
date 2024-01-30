e5352ef9c81b6174e5cde1b753fd0b8b341fe21e
Abdullah-Suri <Abdullah-Suri@users.noreply.github.com>
2024-01-30 10:46:11 +0000

    deploy: a7cd12652226f5a395b5977e029edbe117254b39

--- assets/About-CEv0KjQJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,a as o}from"./index-sEzs_Vle.js";const r={data(){return{}}};function a(n,c,s,_,p,u){return e(),o("div",null," About ")}const f=t(r,[["render",a]]);export{f as default};
