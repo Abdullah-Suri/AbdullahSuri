a5e31bdef623874641ce92cc74d6863ca20d9e83
Abdullah-Suri <m.abdullahsuri@gmail.com>
2024-01-30 15:14:32 +0500

    build upload

---
+++ dist/assets/About-G0gcP5fd.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,a as o}from"./index-duyaZsnk.js";const r={data(){return{}}};function a(n,c,s,_,p,u){return e(),o("div",null," About ")}const f=t(r,[["render",a]]);export{f as default};
