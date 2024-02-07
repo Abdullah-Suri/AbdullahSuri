fde55e83171fe578aa5a9e82e000d1b54e6e5da1
Abdullah-Suri <m.abdullahsuri@gmail.com>
2024-02-07 18:28:03 +0500

    build

---
+++ dist/assets/About-ncLAm76L.js
@@ -0,0 +1,1 @@
+import{_ as t,a as e,o}from"./index-o13B9Ynb.js";const r={data(){return{}}};function a(n,c,s,_,p,u){return o(),e("div",null," About ")}const f=t(r,[["render",a]]);export{f as default};
